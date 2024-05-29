609985c0bcf69edbb5d144454c0f80b7a18ced13
kaylannemendesdossantos@gmail.com <kaylannemendesdossantos@gmail.com>
2024-05-29 12:47:09 -0300

    update

---
+++ desafio2/script.js
@@ -0,0 +1,8 @@
+myFunction();{
+    let x = document.getElementById('contexText');
+    if (x.style.display == 'none') {
+        x.style.display = 'block'
+    } else {
+        x.style.display = 'none';
+    }
+}
