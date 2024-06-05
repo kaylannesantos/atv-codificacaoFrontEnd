51b9c7c104e2554ca13a9c91999519ff7cfe0174
kaylannemendesdossantos@gmail.com <kaylannemendesdossantos@gmail.com>
2024-06-05 13:02:16 -0300

    update

---
+++ desafio2/js/script.js
@@ -0,0 +1,29 @@
+function topo(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior:'smooth'
+    })
+}
+
+function login(){
+    let logado = 0;
+    let usuario = document.getElementsByName('username')[0].value;
+    usuario = usuario.toLowerCase();
+
+    let senha = document.getElementById('password').value;
+
+    if (usuario == 'admin' && senha == '@dmin') {
+        window.location = 'index.html';
+        logado = 1
+    }
+
+    if (logado == 0) {
+        alert('Usuário ou Senha incorretos. Tente novamente.');
+    }
+}
+
+function cadastro(){
+    alert('Cadastrado com sucesso.');
+    window.location.href = 'index.html';
+}
--- desafio2/script.js
+++
@@ -1,8 +0,0 @@
-myFunction();{
-    let x = document.getElementById('contexText');
-    if (x.style.display == 'none') {
-        x.style.display = 'block'
-    } else {
-        x.style.display = 'none';
-    }
-}
